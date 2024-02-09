d498597c0966811b17bcfb9352082512d2b9915a
manuel.sanchez.torres <manuel.san.torr@gmail.com>
2024-02-09 10:09:02 +0100

    Including basic html template, htmx and gofiver

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		Views: html.New("./views", ".html"),
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+		})
+	})
+	log.Fatal(app.Listen(":3000"))
+}
---
+++ src/api/SerieController.go
@@ -0,0 +1,11 @@
+package api
+
+import (
+	"time"
+)
+
+type serie struct {
+	Name:string
+	InclussionDate:time
+	InterestLevel:int
+}
